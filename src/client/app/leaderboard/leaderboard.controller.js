7e97a83e7161ea342708ea49bba915ba3af8c816
Josh Crawford <josh@crawford.cc>
2015-10-19 05:19:08 -0700

    combined overall rank with events, style update

--- src/client/app/leaderboard/leaderboard.controller.js
+++ src/client/app/leaderboard/leaderboard.controller.js
@@ -12,13 +12,15 @@
         vm.messageCount = 0;
         vm.title = 'Leaderboard';
         vm.selectedDivision = null;
-        vm.selectedEvent = 'overallRank';
+        vm.selectedEvent = 'overall';
         
         var ref = new Firebase("https://boiling-fire-216.firebaseio.com/divisions");
         vm.divisions = $firebaseArray(ref);
         vm.divisions.$loaded(function (divisions) {
             vm.selectedDivision = divisions[0].name;
+            //buildOverall();
         });
+        vm.divisions.$watch(buildOverall);
         
         vm.showDivision = function(divisionName){
             vm.selectedDivision = divisionName;
@@ -26,6 +28,33 @@
         
         vm.showEvent = function(eventName){
             vm.selectedEvent = eventName;
+        }
+        
+        function buildOverall(){
+            vm.divisions.forEach(function(division){
+                var overallEvent = {scores: [], name: 'overall'};
+                division.events.forEach(function(event){
+                    event.scores.forEach(function(score){
+                        var isUpdated = false;
+                        overallEvent.scores.forEach(function(overallEventScore){
+                            if (overallEventScore.athleteName === score.athleteName) {
+                                overallEventScore.value += score.value;
+                                isUpdated = true;
+                            }
+                        });
+                        
+                        if (!isUpdated){
+                            var overallEventScore = {athleteName: score.athleteName, value: score.value}
+                            overallEvent.scores.push(overallEventScore);
+                        }
+                    })
+                })
+                division.events = division.events.filter(function(event){
+                    if (event.name === 'overall') return false;
+                    return true;
+                });
+                division.events.push(overallEvent);
+            })
         }
     }
 })();
