a416c67678cefd6889cdbfb285f082bed794f163
Josh Crawford <josh@crawford.cc>
2015-10-23 21:04:29 -0700

    fixed overall to have points not total score, added amprap score to view

--- src/client/app/leaderboard/leaderboard.controller.js
+++ src/client/app/leaderboard/leaderboard.controller.js
@@ -18,7 +18,6 @@
         vm.divisions = $firebaseArray(ref);
         vm.divisions.$loaded(function (divisions) {
             vm.selectedDivision = divisions[0].name;
-            //buildOverall();
         });
         vm.divisions.$watch(buildOverall);
         
@@ -32,29 +31,37 @@
         
         function buildOverall(){
             vm.divisions.forEach(function(division){
-                var overallEvent = {scores: [], name: 'overall'};
-                division.events.forEach(function(event){
-                    event.scores.forEach(function(score){
-                        var isUpdated = false;
-                        overallEvent.scores.forEach(function(overallEventScore){
-                            if (overallEventScore.athleteName === score.athleteName) {
-                                overallEventScore.value += score.value;
-                                isUpdated = true;
-                            }
-                        });
-                        
-                        if (!isUpdated){
-                            var overallEventScore = {athleteName: score.athleteName, value: score.value}
-                            overallEvent.scores.push(overallEventScore);
-                        }
-                    })
-                })
+                var overallEvent = {scores: [], name: 'overall', measure: 'overall', sort: 'ascending'};
                 division.events = division.events.filter(function(event){
                     if (event.name === 'overall') return false;
                     return true;
                 });
+                
+                division.events.forEach(function(event){
+                    event.scores.sort(function(a, b){
+                        var equality = 0;
+                        if (a.value < b.value) equality = -1;
+                        if (a.value > b.value) equality = 1;
+                        
+                        if (event.sort === 'ascending') equality *= -1;
+                        
+                        return equality;
+                    });
+                    
+                    event.scores.forEach(function(score, index, array){                 
+                        var overallIndex = overallEvent.scores
+                            .map(function(score) {return score.athleteName; })
+                            .indexOf(score.athleteName);
+                            
+                        if (overallIndex === -1) {
+                            overallEvent.scores.push({athleteName: score.athleteName, value: index+1});
+                        } else {
+                            overallEvent.scores[overallIndex].value += index+1;
+                        }
+                    });
+                });            
                 division.events.push(overallEvent);
-            })
+            });
         }
     }
 })();
